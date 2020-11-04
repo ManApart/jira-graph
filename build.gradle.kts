dca4c398a8cd615383f301133e6f907caf25c18e
ManApart <raustinkucera@gmail.com>
2020-11-04 07:32:35 -0500

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+group = "me.austin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+dependencies {
+    implementation("org.graphstream:gs-core:2.0")
+    implementation("org.graphstream:gs-ui-swing:2.0")
+//    implementation("org.graphstream:gs-ui-javafx:2.0")
+    implementation("org.graphstream:gs-algo:2.0")
+    testImplementation(kotlin("test-junit5"))
+}
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
